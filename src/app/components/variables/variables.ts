d21d7cd59a9922b72e26ec3afbc19afc82af605a
Joseph Sung <chengyu.sung@gmail.com>
2025-12-10 21:40:30 +0800

    Part 2; 15:27

---
+++ src/app/components/variables/variables.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { email } from '@angular/forms/signals';
+
+@Component({
+  selector: 'app-variables',
+  imports: [],
+  templateUrl: './variables.html',
+  styleUrl: './variables.css',
+})
+export class Variables {
+  courseName: string = 'Angular 21';
+  currentVersion = "v.21";
+
+  rollNo: number = 121;
+  productPrice = 1200.50;
+
+  isActive: boolean = false;
+  isPresent = true;
+
+  currentDate: Date = new Date();
+
+  cityList: string[] = ['Pune', 'Mumbai', 'Jaipur'];
+
+  rollNoArray: number[] = [111, 112, 113, 114, 115];
+
+  studentObj = {
+    name: 'Chetan',
+    modbile: '9900990000',
+    email: 'chetan@gmail.com'
+  }
+
+  studentList = [
+    { name: 'ABC', city: 'Pune'},
+    { name: 'MNQ', city: 'Mumbai'},
+    { name: 'QWE', city: 'Jaipur'}
+  ]
+
+}
