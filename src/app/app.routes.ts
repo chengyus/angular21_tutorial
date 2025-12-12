7b4f7c182b789078fec6a6a9ea7de3260a33c6b8
Joseph Sung <chengyu.sung@gmail.com>
2025-12-12 17:13:59 +0800

    part 3 started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'databinding',
+    component: DataBinding
+  },
+  {
+    path: 'signal',
+    component: Signal
+  },
+  {
+    path: 'variables',
+    component: Variables
+  }
+];
